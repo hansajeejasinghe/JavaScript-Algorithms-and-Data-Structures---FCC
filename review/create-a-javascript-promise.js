f073453b583a9ee52999e6506e5bb7776f7aefcf
Hansajee Jasinghe <122422697+hansajeejasinghe@users.noreply.github.com>
2023-03-17 01:25:58 +0530

    Delete review directory

--- review/create-a-javascript-promise.js
+++
@@ -1,6 +0,0 @@
-const makeServerRequest = new Promise((resolve, reject) => {
-
-});
-
-//Promise is a constructor function, so you need to use the new keyword to create one.
-
--- review/create-an-export-fallback-with-export-default.js
+++
@@ -1,5 +0,0 @@
-export default function subtract(x, y) {
-  return x - y;
-}
-
-//you cannot use export default with var, let, or const
