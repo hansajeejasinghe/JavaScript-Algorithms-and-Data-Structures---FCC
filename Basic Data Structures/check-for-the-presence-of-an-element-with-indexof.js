7173dcc93214ffaa59a3aceae3d7dc521f0a08a2
Hansajee Jasinghe <122422697+hansajeejasinghe@users.noreply.github.com>
2023-03-30 21:28:24 +0530

    Add files via upload

---
+++ Basic Data Structures/check-for-the-presence-of-an-element-with-indexof.js
@@ -0,0 +1,28 @@
+
+function quickCheck(arr, elem) {
+  // Only change code below this line
+  if (arr.indexOf(elem) >= 0){
+    return true;
+  } else {
+    return false;
+  }
+  // Only change code above this line
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+//or
+
+/*
+function quickCheck(arr, elem) {
+  // Only change code below this line
+  if (arr.indexOf(elem) === -1){
+    return false;
+  } else {
+    return true;
+  }
+  // Only change code above this line
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+*/
